07dc24dbfdaacf544a74bbbea8b71a5ba5c4d89e
Viet <viet@example.com>
2026-03-22 21:46:07 +0700

    new

---
+++ src/main/java/vn/com/micareer/controller/HomeServlet.java
@@ -0,0 +1,24 @@
+package vn.com.micareer.controller;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = { "/home"})
+public class HomeServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+        throws ServletException, IOException {
+            
+        // Dữ liệu cứng cho việc làm nổi bật
+        
+        req.setAttribute("pageTitle", "Careers - miCareer");
+
+        req.getRequestDispatcher("/views/pages/home.jsp").forward(req, resp);
+    }
+}
