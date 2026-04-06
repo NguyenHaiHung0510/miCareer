4083213f47816dcfc6e00e7650ecf252811f7d76
Hwang2610-0711v <lh1026a1@gmail.com>
2026-04-06 23:40:14 +0700

    Create Offer.java

---
+++ src/main/java/vn/com/micareer/model/Offer.java
@@ -0,0 +1,29 @@
+package vn.com.micareer.model;
+
+import java.math.BigDecimal;
+
+public class Offer {
+    private long offerId;
+    private long jobAppId;
+    private BigDecimal salary;
+    private String desc;
+    private String stat;
+    private Integer version;
+    private long hrId;
+
+    // Getters and Setters
+    public long getOfferId() { return offerId; }
+    public void setOfferId(long offerId) { this.offerId = offerId; }
+    public long getJobAppId() { return jobAppId; }
+    public void setJobAppId(long jobAppId) { this.jobAppId = jobAppId; }
+    public BigDecimal getSalary() { return salary; }
+    public void setSalary(BigDecimal salary) { this.salary = salary; }
+    public String getDesc() { return desc; }
+    public void setDesc(String desc) { this.desc = desc; }
+    public String getStat() { return stat; }
+    public void setStat(String stat) { this.stat = stat; }
+    public Integer getVersion() { return version; }
+    public void setVersion(Integer version) { this.version = version; }
+    public long getHrId() { return hrId; }
+    public void setHrId(long hrId) { this.hrId = hrId; }
+}
